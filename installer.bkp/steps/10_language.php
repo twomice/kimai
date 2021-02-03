64a068f68d0115ade9b60541f354a28ddc8b2e91
Allen Shaw <allen@joineryhq.com>
2021-02-03 20:15:14 +0000

    Remove installer/ and updater/ directories. See master for latest versions.

--- installer.bkp/steps/10_language.php
+++
@@ -1,8 +0,0 @@
-<form id="lang">
-    <div id="lang_sel">
-        <span class="lang" onclick="lang_selected('en'); return false;">English</span>
-        <span class="lang" onclick="lang_selected('de'); return false;">Deutsch</span>
-        <span class="lang" onclick="lang_selected('bg'); return false;">български</span>
-    </div>
-</form>
-<div style="clear:both;"></div>
